1deea36bed318dfb3f8e5aeb60c594ca8e3458ad
Matis Masters <matis@craftedcode.com>
2016-03-05 02:27:21 -0300

    Add reading calculations

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,41 @@
 class Reading < ActiveRecord::Base
+  S_RATIO = 31.02
+  VOLTAGE = 230
+
+  def cad_value
+    raw * 0.0048
+  end
+
+  def amper
+    (raw * S_RATIO)/1000
+  end
+
+  def kwatts
+    watts/1000
+  end
+
+  def watts
+    (VOLTAGE * amper)
+  end
+
+  def voltage
+    VOLTAGE
+  end
+
+  def to_s
+    "\r\n" +
+    "------====== MrWattson ======------\r\n" +
+    "\r\n" +
+    "Voltaje: #{VOLTAGE}\r\n" +
+    "Sensor: #{raw}\r\n" +
+    "Amper: #{amper}\r\n" +
+    "Watts: #{watts}\r\n" +
+    "KWatts: #{kwatts}\r\n"
+  end
+
+  def self.print_all
+    all.each do |reading|
+      puts reading.to_s
+    end
+  end
 end
